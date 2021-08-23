5cca27b1a4f9033dfa2f5fea057fdec370ecd670
Martin Rothe <m.rothe@node4.co.uk>
2021-08-23 16:42:26 +0100

    Improved liux distro detection

--- docker_install.sh
+++ docker_install.sh
@@ -1,11 +1,47 @@
-if grep -i debian /etc/os-release>/dev/null; then
+if grep -i ubuntu /etc/os-release>/dev/null; then
+    echo "Ubuntu Detected.. Installing"
+    apt remove -y docker docker-engine docker.io containerd runc
+    apt install -y apt-transport-https ca-certificates curl gnupg
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+    echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    apt update
+    apt install -y docker-ce docker-ce-cli containerd.io
+
+    curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    chmod +x /usr/local/bin/docker-compose
+    ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+elif grep -i debian /etc/os-release>/dev/null; then
+    echo "Debian Detected.. Installing"
     apt remove -y docker docker-engine docker.io containerd runc
     apt install -y apt-transport-https ca-certificates curl gnupg
     curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
     echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
     apt update
     apt install -y docker-ce docker-ce-cli containerd.io
+
     curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     chmod +x /usr/local/bin/docker-compose
     ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+elif grep -i photon /etc/os-release>/dev/null; then
+    echo "Docker already installed by default in PhotosON - Skipping to Docker Compose"
+    curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    chmod +x /usr/local/bin/docker-compose
+    ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+elif grep -i CentOS /etc/os-release>/dev/null; then
+    echo "CentOS Detected.. Installing"
+    yum remove -y docker docker-engine docker.io containerd runc
+    yum install -y yum-utils
+    yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+    yum update
+    yum install -y docker-ce docker-ce-cli containerd.io
+
+    curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    chmod +x /usr/local/bin/docker-compose
+    ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+else
+    echo "OS Not supported"
 fi
