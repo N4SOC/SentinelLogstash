832b0e534ea0d2a64ba1c9ab7ba857be56017608
mrrothe <martin@rothe.uk>
2022-11-02 15:01:40 +0000

    Add IDS check

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ workspaceID = "123abc"
 workspaceKey = "abc123"
 
 collectors = [
-    {"name": "fortigate", "port": "514", "proto": "udp"}
-    #{{"name": "fortigate", "port": "514", "proto": "udp", "table": "fortigatenew"}
+    {"name": "fortigate", "port": "514", "proto": "udp"},
+    {"name": "thycotic", "port": "514", "proto": "udp", "table": "secretserver"},
+    {"name": "ids", "interface": "eth1"}
 ]
--- deploy.py
+++ deploy.py
@@ -9,27 +9,29 @@ args = {
     "environment_name": config.envName,
     "workspaceID": config.workspaceID,
     "workspaceKey": config.workspaceKey,
-    "table":None
+    "table": None
 }
 for collector in config.collectors:
-    if "table" in collector:
-        args["table"]=collector["table"]
-    else:
-        args["table"]=None
-    if os.path.isdir(f"./{collector['name']}"):
-        if collector['proto'] == "tcp":
-            ports = [f"{collector['port']}:514"]
+    args["table"] = None
+    if "table" in collector:  # If custom table is defined for collector
+        args["table"] = collector["table"]
+    if os.path.isdir(f"./{collector['name']}"):  # Confirm collector exists
+        if collector['name'] == "ids":
+            print("Audomtaed IDS deplotyment not yet available")
         else:
-            ports = [f"{collector['port']}:514/udp"]
+            if collector['proto'] == "tcp":
+                ports = [f"{collector['port']}:514"]
+            else:  # If syslog is UDP
+                ports = [f"{collector['port']}:514/udp"]
 
-        service = {"build": {
-            "context": f"./{collector['name']}",
-            "args": args
-        },
-            "image": f"{collector['name']}_sentinel",
-            "ports": ports
-        }
-        services[collector['name']] = service
+            service = {"build": {
+                "context": f"./{collector['name']}",
+                "args": args
+            },
+                "image": f"{collector['name']}_sentinel",
+                "ports": ports
+            }
+            services[collector['name']] = service
     else:
         print(f"Collector not found: {collector['name']}, please check configuration")
 yml = yaml.dump({"services": services})
@@ -37,14 +39,14 @@ yml = yaml.dump({"services": services})
 with open("docker-compose.yml", "w") as f:
     f.write(yml)
 
-buildcmd=subprocess.run(["docker", "compose", "build"])
-if buildcmd.returncode==0:
+buildcmd = subprocess.run(["docker", "compose", "build"])
+if buildcmd.returncode == 0:
     print(f"Build Successful - {len(services)} collectors built")
 else:
     print("Build Failed")
 
-runcmd=subprocess.run(["docker", "compose", "up", "-d"])
-if runcmd.returncode==0:
+runcmd = subprocess.run(["docker", "compose", "up", "-d"])
+if runcmd.returncode == 0:
     print(f"Execution Successful - {len(services)} collectors running")
 else:
     print("Execution Failed")
