1b1f1f32d7df693b0432d31a91e8815e6c104490
mrrothe <martin@rothe.uk>
2023-01-23 16:47:17 +0000

    Remove lab keys and cycle

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-envName = "belmont"
-workspaceID = "91731dd8-8ffa-4105-9835-53ae5129ad2b"
-workspaceKey = "P0T/AYv5zKqw3AKWzYF3EF3pIqEuPfxThXe3bazvAO4mzMh7lCUKyXjeW+YkfsIGOSEykxQnjwjl3pcRbQY7vg=="
+envName = ""
+workspaceID = ""
+workspaceKey = ""
 
 collectors = [
     {"name": "paloalto", "port": "9514", "proto": "udp"},
